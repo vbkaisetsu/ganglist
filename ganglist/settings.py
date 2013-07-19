a8694ca856fc46ff6b5e0fcb97daab0ef2f9ddf4
Koichi Akabe <vbkaisetsu@gmail.com>
2013-07-19 10:00:37 +0900

    simplify settings loader

--- ganglist/settings.py
+++ ganglist/settings.py
@@ -7,94 +7,49 @@ import configparser
 from ganglist import config
 
 
-class Options:
+class Options: pass
 
-	def __init__(self):
-		# default values for command-line options
-		self.DEFAULT_WIDTH = 30
-		self.DEFAULT_HEIGHT = 5
-		self.DEFAULT_INTERVAL = 60
-		self.DEFAULT_SHOWUSERS = True
-		self.DEFAULT_INLINE = False
-		self.DEFAULT_COLORING = True
-
-		# minimum values for command-line options
-		self.MIN_WIDTH = 1
-		self.MIN_HEIGHT = 1
-		self.MIN_INTERVAL = 10
-
-		# maximum values for command-line options
-		self.MAX_WIDTH = 100
-		self.MAX_HEIGHT = 100
-		self.MAX_INTERVAL = 600
-
-
-class Environment:
-
-	def __init__(self):
-		# data directory
-		self.DATADIR = config.LOCAL_STATE_DIR + "/log/ganglist"
-
-		# host list
-		self.HOSTS = []
-
+class Environment: pass
 
 class Settings:
 
-
 	def __init__(self):
-	
+
 		import os
 
 		self.options = Options()
 		self.environment = Environment()
 		self.colorMap = {} # {string name: int colorno}
 
-		HOME_DIR = os.environ["HOME"]		
+		HOME_DIR = os.environ["HOME"]
 		CONF_BASENAME = "ganglist.conf"
-		
+
 		conffile = configparser.ConfigParser()
-		
+
 		if os.path.exists(config.SYSTEM_CONFIG_DIR + "/" + CONF_BASENAME):
 			conffile.read(config.SYSTEM_CONFIG_DIR + "/" + CONF_BASENAME)
 		if os.path.exists(HOME_DIR + "/." + CONF_BASENAME):
 			conffile.read(HOME_DIR + "/." + CONF_BASENAME)
 
 		if "Options" in conffile:
-			if "DEFAULT_WIDTH" in conffile["Options"]:
-				self.options.DEFAULT_WIDTH = conffile["Options"].getint("DEFAULT_WIDTH")
-			if "DEFAULT_HEIGHT" in conffile["Options"]:
-				self.options.DEFAULT_HEIGHT = conffile["Options"].getint("DEFAULT_HEIGHT")
-			if "DEFAULT_INTERVAL" in conffile["Options"]:
-				self.options.DEFAULT_INTERVAL = conffile["Options"].getint("DEFAULT_INTERVAL")
-			if "DEFAULT_SHOWUSERS" in conffile["Options"]:
-				self.options.DEFAULT_SHOWUSERS = conffile["Options"].getboolean("DEFAULT_SHOWUSERS")
-			if "DEFAULT_INLINE" in conffile["Options"]:
-				self.options.DEFAULT_INLINE = conffile["Options"].getboolean("DEFAULT_INLINE")
-			if "MIN_WIDTH" in conffile["Options"]:
-				self.options.MIN_WIDTH = conffile["Options"].getint("MIN_WIDTH")
-			if "MIN_HEIGHT" in conffile["Options"]:
-				self.options.MIN_HEIGHT = conffile["Options"].getint("MIN_HEIGHT")
-			if "MIN_INTERVAL" in conffile["Options"]:
-				self.options.MIN_INTERVAL = conffile["Options"].getint("MIN_INTERVAL")
-			if "MAX_WIDTH" in conffile["Options"]:
-				self.options.MAX_WIDTH = conffile["Options"].getint("MAX_WIDTH")
-			if "MAX_HEIGHT" in conffile["Options"]:
-				self.options.MAX_HEIGHT = conffile["Options"].getint("MAX_HEIGHT")
-			if "MAX_INTERVAL" in conffile["Options"]:
-				self.options.MAX_INTERVAL = conffile["Options"].getint("MAX_INTERVAL")
+			self.options.DEFAULT_WIDTH = conffile["Options"].getint("DEFAULT_WIDTH", 30)
+			self.options.DEFAULT_HEIGHT = conffile["Options"].getint("DEFAULT_HEIGHT", 5)
+			self.options.DEFAULT_INTERVAL = conffile["Options"].getint("DEFAULT_INTERVAL", 60)
+			self.options.DEFAULT_SHOWUSERS = conffile["Options"].getboolean("DEFAULT_SHOWUSERS", True)
+			self.options.DEFAULT_INLINE = conffile["Options"].getboolean("DEFAULT_INLINE", False)
+			self.options.DEFAULT_COLORING = conffile["Options"].getboolean("DEFAULT_COLORING", True)
+			self.options.MIN_WIDTH = conffile["Options"].getint("MIN_WIDTH", 1)
+			self.options.MIN_HEIGHT = conffile["Options"].getint("MIN_HEIGHT", 1)
+			self.options.MIN_INTERVAL = conffile["Options"].getint("MIN_INTERVAL", 10)
+			self.options.MAX_WIDTH = conffile["Options"].getint("MAX_WIDTH", 100)
+			self.options.MAX_HEIGHT = conffile["Options"].getint("MAX_HEIGHT", 100)
+			self.options.MAX_INTERVAL = conffile["Options"].getint("MAX_INTERVAL", 600)
 
 		if "Environment" in conffile:
-			if "DATADIR" in conffile["Environment"]:
-				self.environment.DATADIR = conffile["Environment"]["DATADIR"]
-			if "HOSTS" in conffile["Environment"]:
-				self.environment.HOSTS = conffile["Environment"]["HOSTS"].split()
+			self.environment.DATADIR = conffile["Environment"].get("DATADIR", config.LOCAL_STATE_DIR + "/log/ganglist")
+			self.environment.HOSTS = conffile["Environment"].get("HOSTS", "").split()
 
 		if "Colors" in conffile:
-			if "USER" in conffile["Colors"]:
-				self.colorMap["USER"] = int(conffile["Colors"]["USER"])
-			if "SYSTEM" in conffile["Colors"]:
-				self.colorMap["SYSTEM"] = int(conffile["Colors"]["SYSTEM"])
-			if "CACHE" in conffile["Colors"]:
-				self.colorMap["CACHE"] = int(conffile["Colors"]["CACHE"])
-
+			self.colorMap["USER"] = conffile["Colors"].getint("USER", 1)
+			self.colorMap["SYSTEM"] = conffile["Colors"].getint("SYSTEM", 2)
+			self.colorMap["CACHE"] = conffile["Colors"].getint("CACHE", 3)
